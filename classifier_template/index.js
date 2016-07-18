3558290355862d1d6498fed2f54de878eaf18b4c
Jussi Hamalainen <jussi@nollaversio.fi>
2016-07-18 11:54:30 +0300

    Fixing scaffolding gulp stuff

--- classifier_template/index.js
+++ classifier_template/index.js
@@ -1,3 +1,14 @@
+/* 
+Instantiate this template using cmd line command:
+'gulp classifier --name MyClassifier'
+*/
+
+// Defaults deps
+var Promise = require('bluebird');
+var lwip = require('lwip');
+var _ = require('lodash');
+var fs = require('fs');
+
 var getFeatureVectors = require('./getFeatureVectors');
 var transformImage = require('./transformImage');
 var concludePosition = require('./concludePosition');
@@ -16,4 +27,17 @@ module.exports = function() {
 		getFeatureVectors: getFeatureVectors,
 		concludePosition: concludePosition
 	}
+}
+
+
+/* Helper function for reading image from disk */
+
+function getImageData(imagepath) {
+	return new Promise(function(resolve, reject) {
+			lwip.open(imagepath, function(err, image) {
+				if (err) return reject(err);
+				resolve(image);
+			});
+	});
+
 }
--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,12 @@
 var gulp = require('gulp');
 
+// This gulp script allows to scaffold classifiers
+// usage cmd line: 'gulp classifier --name MyNewClassifier'
+
 gulp.task('classifier', function() {
   // place code for your default task here
   gulp.src([
   	'classifier_template/**/*'
-  ]).pipe(gulp.dest('classifiers/testiclassifier'));
+  ]).pipe(gulp.dest('classifiers/' + process.argv.slice()[4]));
 
 });
